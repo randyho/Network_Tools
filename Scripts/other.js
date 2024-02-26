b49035d741f3df169097e32ce82f269161a7b1ac
Randy <randyho@outlook.com>
2024-02-26 19:35:18 +0800

    Create other.js

---
+++ Scripts/other.js
@@ -0,0 +1,19 @@
+const url = $request.url;
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
+
+
+if ($request.url.includes("argus/api/v3/user/getaccountpage")) {
+  let div_ad = document.querySelectorAll('div.mx-auto[style]')
+    for (i = 0; i < div_ad.length; i++) {
+        if (div_ad[i].querySelectorAll('[target=\'_blank\']').length >= 1) {
+            div_ad[i].style.height = '0px'
+        }
+    }
+  $.done({});
+} else if ($request.url.includes("argus/api/v1/assembly/toolbar")) {
+  var Q = JSON.parse($response.body);
+  Q.Data.Toolbar.Adv = {};
+  $done({body : JSON.stringify(Q)});
+}
+
